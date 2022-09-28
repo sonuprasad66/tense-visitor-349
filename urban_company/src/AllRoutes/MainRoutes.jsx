c8681345329b79da5bcba96c59e3e66c62d30a83
omkundu <omkundu156@gmail.com>
2022-09-28 23:03:18 +0530

    second-Changes-day-2

--- urban_company/src/AllRoutes/MainRoutes.jsx
+++ urban_company/src/AllRoutes/MainRoutes.jsx
@@ -1,32 +1,18 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-
-
-
-
 import Package from "../Components/Package/Package";
+
 import Home from "./Home";
-
-
+import { Services } from "./Services";
 
 const MainRoutes = () => {
   return (
-    
-      <Routes>
-
-
-      {/* <Route path="/MainLandingPage" element={<MainLandingPage/>} /> */}
-        <Route path="/" element={<Home />} />
-        <Route path="/package" element={<Package />} />
-
-
-      </Routes>
-    
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/package" element={<Package />} />
+      <Route path="/service" element={<Services />} />
+    </Routes>
   );
 };
 
 export default MainRoutes;
-
-
-
-
