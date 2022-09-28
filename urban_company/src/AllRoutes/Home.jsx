6a329a389a8c29678a7de02a8a0a70f3d3b1b4d9
omkundu <omkundu156@gmail.com>
2022-09-28 18:26:56 +0530

    day-2-done

--- urban_company/src/AllRoutes/Home.jsx
+++ urban_company/src/AllRoutes/Home.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div>navbar 
-  slider
+  return <div>
   </div>;
 };
 
--- urban_company/src/AllRoutes/MainRoutes.jsx
+++ urban_company/src/AllRoutes/MainRoutes.jsx
@@ -7,7 +7,7 @@ const MainRoutes = () => {
   return (
     <div>
       <Routes>
-      <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home/>} />
         <Route path="/MainLandingPage" element={<MainLandingPage/>} />
       </Routes>
     </div>
