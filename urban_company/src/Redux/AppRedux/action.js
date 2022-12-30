8244bec7962ae29c6255d1bb04da3daf4a9baa0b
MD OWAIS ATHAR <owais.athar.8092@gmail.com>
2022-12-30 15:31:41 +0530

    Api updated to render

--- urban_company/src/Redux/AppRedux/action.js
+++ urban_company/src/Redux/AppRedux/action.js
@@ -3,7 +3,7 @@ import axios from "axios"
 
 export const getService =(params) =>(dispatch)=>{
     dispatch({type : types.GET_SERVICE_REQUEST})
-    return axios.get("https://urban-company-api.herokuapp.com/api/service", params)
+    return axios.get("https://urban-api-vp0s.onrender.com/service", params)
     
     .then((res)=>{
         dispatch({type : types.GET_SERVICE_SUCCESS, payload:res.data})
